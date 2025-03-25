a3bc47535c3c17b21506dc26d7e06ba1bb9b58db
art <147410114+xduyu@users.noreply.github.com>
2025-03-25 18:07:22 +0500

    Add files via upload

---
+++ js/5app.js
@@ -0,0 +1,56 @@
+// Синхронный код
+
+
+
+// console.log('1')
+// console.log('2')
+// console.log('3')
+
+
+
+
+// Асхинронный код
+
+// console.log('start')
+
+// setTimeout(() => {
+//     console.log('setTimeout')
+// }, 1000)
+
+// console.log('end')
+
+// console.log(1)
+
+// // set  Timeout(() => {
+// document.getElementById('btn').addEventListener('click', () => {
+//     console.log(123)
+// })
+// // }, 1000)
+
+
+// console.log(321)
+
+
+// а. загрузка картинки
+
+
+document.getElementById('btn').addEventListener('click', () => {
+    const img = document.createElement('img')
+    img.src = 'https://allterra.ru/upload/iblock/3e0/b8iu701mkpbeivrkjzgn7d5gw71tc791/IMG-Academy-1.jpg'
+    img.addEventListener('load', () => {
+        document.body.append(img)
+    })
+
+    img.addEventListener('error', () => {
+        alert('err')
+    })
+
+})
+
+function main() {
+    setTimeout(function gridSet() {
+        console.log('hello')
+    }, 123)
+}
+
+main()
